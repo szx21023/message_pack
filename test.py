9bb2df7771032e56364ba8c57264dc0d5d7e6d1c
szx21023 <szx21023@gmail.com>
2025-03-28 14:55:38 +0800

    add unit test

--- test.py
+++ test.py
@@ -1,28 +1,50 @@
 from encoder_and_decoder import msgpack_encode, msgpack_decode
 import msgpack
 
-def test_msgpack_encode():
-    # 測試數據
-    test_data = {"name": "Alice", "age": 30, "is_student": False, "scores": [90, 80, 70]}
-    
+import pytest
+
+@pytest.mark.parametrize(
+    "test_data",  # 測試欄位
+    [
+        ({}), # 第一組測試資料
+        ({'a': 1}), # 第二組測試資料
+        ({'a': -1}), # 第三組測試資料
+        ({'a': '1'}), # 第四組測試資料
+        ({'a': []}), # 第五組測試資料
+        ({'a': 1, 'b': '4', 'C': True}), # 第六組測試資料
+        ({'name': 'test', 'age': 40, 'address': 'ameriva', 'height': 180.4, 'weight': 74.855432542354235, 'is_student': False, 'scope': [80, 70, 60]}), # 第七組測試資料
+        ({'nirieniwbgiwerbgiib': 'rweiogurioewgigoihuiih', 'aaa': None, 'tt': {'t': 1, 'b': 2}}) # 第八組測試資料
+    ]
+)
+def test_msgpack_encode(test_data):
     # 使用自定義的 msgpack_encode 函數進行編碼
     packed_data = msgpack_encode(test_data)
     
     # 使用 msgpack 庫進行編碼
     expected_data = msgpack.packb(test_data)
     
-    # 比較兩者的結果
+    # 自定義 encode 和第三方 encode 的結果是否相同
     assert packed_data == expected_data, f"Expected {expected_data}, but got {packed_data}"
 
-def test_msgpack_decode():
-    # 測試數據
-    test_data = {"name": "Alice", "age": 30, "is_student": False, "scores": [90, 80, 70]}
-    
+@pytest.mark.parametrize(
+    "test_data",  # 測試欄位
+    [
+        ({}), # 第一組測試資料
+        ({'a': 1}), # 第二組測試資料
+        ({'a': -1}), # 第三組測試資料
+        ({'a': '1'}), # 第四組測試資料
+        ({'a': []}), # 第五組測試資料
+        ({'a': 1, 'b': '4', 'C': True}), # 第六組測試資料
+        ({'name': 'test', 'age': 40, 'address': 'ameriva', 'height': 180.4, 'weight': 74.855432542354235, 'is_student': False, 'scope': [80, 70, 60]}), # 第七組測試資料
+        ({'nirieniwbgiwerbgiib': 'rweiogurioewgigoihuiih', 'aaa': None, 'tt': {'t': 1, 'b': 2}}) # 第八組測試資料
+    ]
+)
+def test_msgpack_decode(test_data):
     # 使用自定義的 msgpack_encode 函數進行編碼
     packed_data = msgpack_encode(test_data)
     
     # 使用自定義的 msgpack_decode 函數進行解碼
     decoded_data, _ = msgpack_decode(packed_data)
     
-    # 比較解碼結果與原始數據
+    # 自定義 encode 再自定義 decode 後，與原始數據是否相同
     assert decoded_data == test_data, f"Expected {test_data}, but got {decoded_data}"
